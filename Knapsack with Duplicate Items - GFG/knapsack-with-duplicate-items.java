ddf9fc31be620d70a2dd80e857ad82b3c2677100
Anup Vishwakarma <92694915+anupv23@users.noreply.github.com>
2023-05-26 18:03:12 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -33,23 +33,38 @@ class GFG{
 //User function Template for Java
 
 class Solution{
-    static int knapSack(int N, int W, int val[], int wt[])
-    {int[][] dp=new int[N][W+1];
-    for(int [] row:dp){
-        Arrays.fill(row,-1);
+    static int knapSack(int n, int w, int val[], int wt[])
+    {
+    int[][] dp=new int[n][w+1];
+    // for(int [] row:dp){
+    //     Arrays.fill(row,-1);
+    // }
+    //     return fun(N-1,W,val,wt,dp); 
+    // }
+    // static int fun(int ind,int w,int[] val,int [] wt,int[][]dp){
+    //     if(ind==0){
+    //         return ((int)w/wt[0]*val[0]);
+    //     }
+    //     if(dp[ind][w]!=-1) return dp[ind][w];
+    //     int notTake=fun(ind-1,w,val,wt,dp);
+    //     int take=0;
+    //     if(wt[ind]<=w){
+    //         take=val[ind]+fun(ind,w-wt[ind],val,wt,dp);
+    //     }
+    //     return dp[ind][w]=Math.max(take,notTake);
+    // }
+    for(int i=0;i<=w;i++){
+        dp[0][i]=(int)(i/wt[0])*val[0];
     }
-        return fun(N-1,W,val,wt,dp); 
-    }
-    static int fun(int ind,int w,int[] val,int [] wt,int[][]dp){
-        if(ind==0){
-            return ((int)w/wt[0]*val[0]);
+    for(int ind=1;ind<n;ind++){
+        for(int weight=0;weight<=w;weight++){
+            int notTake=dp[ind-1][weight];
+            int take=0;
+            if(wt[ind]<=weight)
+            take=val[ind]+dp[ind][weight-wt[ind]];
+            dp[ind][weight]=Math.max(take,notTake);
         }
-        if(dp[ind][w]!=-1) return dp[ind][w];
-        int notTake=fun(ind-1,w,val,wt,dp);
-        int take=0;
-        if(wt[ind]<=w){
-            take=val[ind]+fun(ind,w-wt[ind],val,wt,dp);
-        }
-        return dp[ind][w]=Math.max(take,notTake);
     }
+    return dp[n-1][w];
+}
 }
