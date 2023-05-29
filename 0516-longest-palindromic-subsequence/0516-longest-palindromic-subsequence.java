b9dce9650c7df4b3c06d3d1d33ce65e1e473037e
Anup Vishwakarma <92694915+anupvishwakarmaa@users.noreply.github.com>
2023-05-29 10:47:33 +0530

    Time: 103 ms (11.55%), Space: 44.4 MB (85.58%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -10,18 +10,21 @@ class Solution {
     }
     public int longestCommonSubsequence(String text1, String text2) {
         int m=text1.length();
-        int n=text2.length();
-        int[][]dp=new int[m+1][n+1];
+        // int[][]dp=new int[m+1][m+1];
+        int[] prev=new int[m+1];
+        int[]curr=new int[m+1];
         for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0 ||j==0) dp[i][j]=0;
+            for(int j=0;j<=m;j++){
+                if(i==0 ||j==0) curr[j]=0;
                 else if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+prev[j-1];
                 }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    curr[j]=Math.max(prev[j],curr[j-1]);
                 }
             }
+            // prev=curr;
+            prev=(int[])(curr.clone());
         }
-        return dp[m][n];
+        return prev[m];
     }
 }
